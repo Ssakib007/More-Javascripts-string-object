5a8b12ec99502fca233038a3b99e44b814a4cf78
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 14:19:12 +0600

    comparing using upper or lower case and trim

---
+++ case.js
@@ -0,0 +1,24 @@
+const school = "Raj UK Uttara Model School";
+
+console.log(school);
+console.log(school.toUpperCase());
+console.log(school.toLowerCase());
+const subject = "chemistry";
+const book = "Chemistry";
+
+if (subject.toLowerCase() === book.toLocaleLowerCase()) {
+  console.log("I am reading book");
+} else {
+  console.log("not reading at all");
+}
+
+// Uppercase --> ABCD
+// Lowercase --> abcd
+
+const drink = "water";
+const liquid = "    water   ";
+if (drink.trim() === liquid.trim()) {
+  console.log("water is life");
+} else {
+  console.log("water in the sea");
+}
