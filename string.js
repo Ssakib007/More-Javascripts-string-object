2c4a0e95c17095261c3aa5a7114683732a83d0a8
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 13:59:34 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,21 @@
+const country = "Bangladesh";
+const division = "Dhaka";
+const district = `Dhaka`;
+const thana = new String("Demra");
+
+console.log(typeof thana);
+console.log(thana);
+
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length);
+console.log(numbers[0]);
+numbers[1] = 10;
+console.log(numbers);
+
+const capital = "Dhaka";
+console.log(capital.length);
+console.log(capital[2]);
+capital[0] = "f";
+console.log(capital);
+// ("strings are immutable");
+// ("arrays are mutable");
