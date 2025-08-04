d5b85180d89e77b89576d485e9d163bf85d2fe70
Ssakib007 <sakib.qbo.uk@gmail.com>
2025-08-04 15:08:22 +0600

    object

---
+++ object.js
@@ -0,0 +1,26 @@
+// primitive, basic
+const age = 21;
+const school = "CUFL";
+const isPassed = true;
+let isDeveloper;
+const subjects = ["bangla", "english", "physics", "math"];
+console.log(isDeveloper);
+
+// object --> non primitive
+const bottle = {
+  brand: "apple",
+  price: 45,
+  color: "white",
+  isClean: false,
+};
+
+const subject = {
+  name: "biology",
+  teacher: "mam",
+  examDate: "30",
+  chapters: ["first", "second", "third"],
+  exams: {
+    name: "final exam",
+    marks: 200,
+  },
+};
